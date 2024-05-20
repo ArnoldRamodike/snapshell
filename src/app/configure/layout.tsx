a72108335c98c752bd62347634073189749bc678
ArnoldRamodike <riccoarnold23@gmail.com>
2024-05-20 18:35:48 +0200

    Cart Steps

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,10 +1,12 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import Steps from '@/components/Steps'
 import React from 'react'
 
 export default function Layout({ children}: Readonly<{children: React.ReactNode}>) {
 
   return (
     <MaxWidthWrapper className='flex-1 flex flex-col '>
+        <Steps/>
         {children}
     </MaxWidthWrapper>
   )
